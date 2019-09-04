ef558c6c11798c174a4c4277d65efce7aaddcd37
Matt Lissick <mattlissick@gmail.com>
2019-09-04 15:33:56 -0500

    base mode

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const cookieSession = require('cookie-session');
 const express = require('express');
 const bodyParser = require('body-parser');
+
  
 const app = express();
 
 app.use(bodyParser.json());
+// need to fix this: app.use(bodyParser.urlendcoded())
  
 app.use(cookieSession({
   name: 'session',
@@ -20,11 +22,26 @@ app.post('/add-click', (req,res) => {
   res.sendStatus(200);
 });
 
+app.post('/add-username', (req, res) => {
+  console.log(req.session);
+  
+  req.session.username = req.session.username || 'no user';
+  req.session.username = req.body.username;
+  res.sendStatus(200);
+});
+
 app.get('/get-clicks', (req, res) => {
   req.session.totalClicks = req.session && req.session.totalClicks || 0;
   const {totalClicks} = req.session;
   res.send({totalClicks});
 });
+
+app.get('/get-username', (req, res) => {
+  console.log(req.session.username)
+  let username = req.session.username;
+  res.send({ username });
+});
+
 
 // App Set //
 const PORT = process.env.PORT || 5000;
--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 class App extends Component {
   state = {
     clickCount: 0,
+    usernameIsEditable: false,
+    username: ''
   }
 
   componentDidMount() {
@@ -30,6 +32,29 @@ class App extends Component {
       });
   }
 
+  getUsername = () => {
+    axios.get('/get-username')
+      .then(response => {
+        this.setState({
+          username: response.data.newUsername,
+        });
+      })
+      .catch(error => {
+        console.log('error making add username post', error);
+      });
+  }
+
+  handleNewEntry = (propertyName, event) => {
+    console.log("in handle new entry");
+    // const newUsername = this.state.username
+    // document.cookie = `username=${newUsername}`
+    this.setState({
+      ...this.state,
+      [propertyName]: event.target.value
+    })
+    console.log(this.state);
+  }
+
   editUsername = () => {
     this.setState({
       usernameIsEditable: true,
@@ -40,6 +65,12 @@ class App extends Component {
     this.setState({
       usernameIsEditable: false,
     });
+    axios.post('/add-username')
+      .then(() => this.getUsername())
+      .catch(error => {
+        console.log('error making add click post', error);
+      });
+
   }
 
   render() {
@@ -47,16 +78,26 @@ class App extends Component {
       <div>
         <center>
           <h1>Click the Cookie!!</h1>
+          <pre>{JSON.stringify(this.state)}</pre>
           <p>
+            Username:
+            {this.state.username}
+          </p>
+          <div>
             {/* Username should go here */}
             {/* The next block of code is conditional rendering.
             Look at the documentation https://reactjs.org/docs/conditional-rendering.html
             if this is new to you. */}
             {this.state.usernameIsEditable ?
-              <button onClick={this.saveUsername}>Save Username</button> :
+              <div>
+                <input type='text' placeholder='add new username' onChange={(event) => { this.handleNewEntry('username', event) }} />
+              
+                <button onClick={this.saveUsername}>Save Username</button> 
+              </div>:
               <button onClick={this.editUsername}>Edit Username</button>
+             
             }
-          </p>
+          </div>
           <p>{this.state.clickCount}</p>
           <span
             role="img"
